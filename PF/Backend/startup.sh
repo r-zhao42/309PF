000430964fe1e2efe53c346e6ea91bef0ac0017b
Ryan Zhao <rzhao42@icloud.com>
2022-11-29 18:14:09 -0500

    got rid of extra docs

--- PF/Backend/startup.sh
+++
@@ -1,6 +0,0 @@
-python3 -m virtualenv venv
-source venv/bin/activate
-pip install -r TFCbackend/requirements.txt
-python3 TFCbackend/manage.py makemigrations
-python3 TFCbackend/manage.py migrate --run-syncdb
-python3 TFCbackend/manage.py createsuperuser
