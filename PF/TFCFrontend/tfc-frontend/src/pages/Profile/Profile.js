e6c99409a2b9dfef8ed7b2315dcc0d52e1be046d
kelian200 <kelian.yvon@gmail.com>
2022-11-30 17:22:17 -0500

    start of profile

---
+++ PF/TFCFrontend/tfc-frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,16 @@
+import './Profile.css';
+
+function Profile() {
+    return (
+        <div className='Profile'>
+            <div className='PaymentList'>
+                <h1>Payment History</h1>
+            </div>
+            <div className='AccountInfo'>
+                <h1>Account Info</h1>
+            </div>
+        </div>
+    );
+  }
+  
+  export default Profile;
