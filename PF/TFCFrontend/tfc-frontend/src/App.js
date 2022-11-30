fbb369c99010d6164ca16d82f333a52a2037f2bd
Ryan Zhao <rzhao42@icloud.com>
2022-11-30 17:20:13 -0500

    inital commit

--- PF/TFCFrontend/tfc-frontend/src/App.js
+++ PF/TFCFrontend/tfc-frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar"
+import Home from "./pages/Home/Home"
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<NavBar />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
