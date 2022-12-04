d5fbb8b0fb08cd8a6116703dc45bd49610c65656
Patricia Ding <patricia.ding@mail.utoronto.ca>
2022-12-04 11:29:29 -0500

    commit

--- PF/TFCFrontend/tfc-frontend/src/App.js
+++ PF/TFCFrontend/tfc-frontend/src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar"
+import Signup from "./pages/Signup/Signup"
 import Home from "./pages/Home/Home"
-import Profile from "./pages/Profile/Profile"
 import './App.css';
 import React from 'react';
 
@@ -11,7 +11,7 @@ function App() {
         <Routes>
             <Route path="/" element={<NavBar />}>
                 <Route index element={<Home />} />
-                <Route path="profile" element={<Profile />} />
+                <Route path="signup" element={<Signup />} />
             </Route>
         </Routes>
     </BrowserRouter>
