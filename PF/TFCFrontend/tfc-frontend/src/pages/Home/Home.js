37bd5d226c2de995cee37675de989d0ee68002f7
Patricia Ding <patricia.ding@mail.utoronto.ca>
2022-12-04 13:42:30 -0500

    homepage styled

--- PF/TFCFrontend/tfc-frontend/src/pages/Home/Home.js
+++ PF/TFCFrontend/tfc-frontend/src/pages/Home/Home.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import "./Home.css";
+import {useNavigate} from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-const Home = () => {
+import "./Home.css";
 
-  const handleClick = () => {
-  };
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/replace-this');
+      };
+    
   
   return (
     <>
-    <div className = "bannerdiv">
-        <div className = "innerdiv">
+    <div className = "outer-div">
+        <div className = "inner-div">
             <h1>Welcome to the Toronto Fitness Club</h1>
+            <br/>
             <Button variant="primary" onClick={handleClick}>
                 Find a Studio Near You
             </Button>
         </div>
     </div>
     </>
-    
 )
 }
 
