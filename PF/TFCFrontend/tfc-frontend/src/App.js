3e3fa772aabbbadde6e86b9a67893b0704e98a8c
Patricia Ding <patricia.ding@mail.utoronto.ca>
2022-12-05 21:56:02 -0500

    nav bar changes

--- PF/TFCFrontend/tfc-frontend/src/App.js
+++ PF/TFCFrontend/tfc-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar"
 import Signup from "./pages/Signup/Signup"
@@ -6,21 +6,29 @@ import Home from "./pages/Home/Home"
 import Profile from "./pages/Profile/Profile"
 import Login from "./pages/Login/Login"
 import StudioDetail from "./pages/StudioDetail/StudioDetail"
+import Logout from "./pages/Logout/Logout"
 import './App.css';
 
 
 function App() {
+  const [ auth, setAuth ] = useState(localStorage.getItem("auth") || 'false')
+  const handleChange = (arg) => {
+    setAuth(arg)
+    localStorage.setItem("auth", arg);
+ }
+ 
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<NavBar />}>
+            <Route path="/" element={<NavBar loginStatus={auth}/>}>
                 <Route index element={<Home />} />
                 
                 <Route path="home" element={<Home />}/>
-                <Route path="signup" element={<Signup />} />
+                <Route path="signup" element={<Signup setLoginStatus={handleChange}/>} />
                 <Route path="profile" element={<Profile />} />
-                <Route path="login" element={<Login />} />
+                <Route path="login" element={<Login setLoginStatus={handleChange}/>} />
                 <Route path="studio/:name" element={<StudioDetail />} />
+                <Route path="logout" element={<Logout setLoginStatus={handleChange}/>} />
             </Route>
         </Routes>
     </BrowserRouter>
---
+++ PF/TFCFrontend/tfc-frontend/src/pages/Logout/Logout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import "./Logout.css";
+
+const Logout = ({ setLoginStatus }) => {
+
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/accounts/logout/', {
+      method: 'POST',
+      mode: 'cors',
+      headers: new Headers({
+        'Authorization': 'Token ' + localStorage.getItem('token'),
+      }),
+    })
+      .then((response) => {
+        if (response.status === 202) {
+          localStorage.setItem('token', '');
+          setLoginStatus('false')
+        }
+
+      });
+  });
+
+  return (
+    <>
+      <div className="outer-div2">
+        <div className="inner-div2">
+          <h4>You have logged out</h4>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Logout;
